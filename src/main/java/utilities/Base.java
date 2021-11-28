4dbe569bae39333ed754629f7188dbfbd51f504d
Yonatan Shiri <yonatanshiri95@gmail.com>
2021-11-28 15:59:48 +0200

    Update Base.java

--- src/main/java/utilities/Base.java
+++ src/main/java/utilities/Base.java
@@ -1,4 +1,13 @@
 package utilities;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.openqa.selenium.WebDriver;
+
 public class Base {
+    public static WebDriver driver;
+    public RequestSpecification httpRequest;
+    public Response response;
+    public JsonPath jsonPath;
 }
