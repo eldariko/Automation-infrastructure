d1dcb743b7f930097e203783ef7bd3ba7261ec5e
Eldariko <eldariko15@gmail.com>
2021-11-29 16:54:00 +0200

    new commit

--- src/main/java/utilities/ManagePages.java
+++ src/main/java/utilities/ManagePages.java
@@ -3,7 +3,7 @@ package utilities;
 public class ManagePages extends Base {
 
     // Mobile: Initiate UK Mortgage Calc app page Objects
-    public static void initUnitConvertor() {
+    public static void initMortgagePage() {
         mortgagePage = new pageObjects.Appium.MortgagePage(mobileDriver);
     }
 
