b99e314957b1b970819ff4a7a87c5f24b1e1d4c8
Eldariko <eldariko15@gmail.com>
2021-11-29 14:19:52 +0200

    new commit

---
+++ src/main/java/extentions/AppiumActions.java
@@ -0,0 +1,64 @@
+package extentions;
+
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AppiumActions extends WebActions {
+
+    @Step("Update Text Element")
+    public static void updateText(MobileElement elem, String text) {
+        wait.until((ExpectedConditions.visibilityOf(elem)));
+        elem.clear();
+        elem.sendKeys(text);
+    }
+
+    //    @Step("Double Tap on Element")
+//    public static void doubleTap(MobileElement elem) {
+//        wait.until(ExpectedConditions.elementToBeClickable(elem));
+//        touchAction.(elem).perform();
+//    }
+    @Step("Single Tap on Element")
+    public static void singleTap(MobileElement elem) {
+        wait.until(ExpectedConditions.elementToBeClickable(elem));
+        touchAction.press(new ElementOption().withElement(elem)).perform();
+    }
+
+    @Step("Long press on Element")
+    public static void longPress(MobileElement elem) {
+        wait.until(ExpectedConditions.elementToBeClickable(elem));
+        touchAction.longPress(new ElementOption().withElement(elem)).perform();
+    }
+
+    @Step("Swipe")
+    public static void swipe(int startX, int startY, int endX, int endY, int duration) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        mobileDriver.(startX, startY, endX, endY, duration);
+    }
+
+    @Step("Zoom Element")
+    public static void zoom(MobileElement elem) {
+        wait.until(ExpectedConditions.visibilityOf(elem));
+
+//        mobileDriver.zoom(elem);
+    }
+
+    @Step("Zoom Element")
+    public static void pinch(MobileElement elem) {
+        wait.until(ExpectedConditions.visibilityOf(elem));
+//        mobileDriver.pinch(elem);
+    }
+
+    @Step("Execute Script")
+    public static void executeScript(String script) {
+        mobileDriver.executeScript(script);
+    }
+
+
+}
---
+++ src/main/java/utilities/ManagePages.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class ManagePages extends Base {
+
+    // Mobile: Initiate UK Mortgage Calc app page Objects
+    public static void initUnitConvertor() {
+        unitConvertorPage = new pageObjects.Appium.UnitConvertorPage(mobileDriver);
+    }
+
+}
